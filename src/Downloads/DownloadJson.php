898ab79d4f1c39d428da93d461db96e651373702
Pavel <pavel.a.bashkatov@yandex.ru>
2019-06-06 21:59:29 +0300

    download json

--- src/Downloads/DownloadJson.php
+++ src/Downloads/DownloadJson.php
@@ -8,12 +8,34 @@
 
 namespace bashkatov\weather\Downloads;
 
+use Apfelbox\FileDownload\FileDownload;
 use bashkatov\weather\Interfaces\Downloadable;
 
 class DownloadJson implements Downloadable
 {
     public function download($forecast)
     {
-        // TODO: Implement save() method.
+        $data = [
+            'time'                => $forecast->time,
+            'temperature'         => $forecast->temperature,
+            'windBearing'         => $forecast->windBearing,
+            'windSpeed'           => $forecast->windSpeed,
+            'summary'             => $forecast->summary,
+            'icon'                => $forecast->icon,
+            'apparentTemperature' => $forecast->apparentTemperature,
+            'dewPoint'            => $forecast->dewPoint,
+            'humidity'            => $forecast->humidity,
+            'pressure'            => $forecast->pressure,
+            'windGust'            => $forecast->windGust,
+            'cloudCover'          => $forecast->cloudCover,
+            'uvIndex'             => $forecast->uvIndex,
+            'visibility'          => $forecast->visibility,
+            'ozone'               => $forecast->ozone,
+        ];
+
+        $json = json_encode($data);
+
+        $fileDownload = FileDownload::createFromString($json);
+        $fileDownload->sendDownload("weather.json");
     }
 }
